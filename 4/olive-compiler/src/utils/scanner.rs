2b05d427c875467dcc9a4ff98a44e93e9acc5267
davidcristian <122480408+davidcristian@users.noreply.github.com>
2023-10-19 15:34:25 +0300

    Refactor scanner

--- 4/olive-compiler/src/utils/scanner.rs
+++ 4/olive-compiler/src/utils/scanner.rs
@@ -20,49 +20,23 @@ pub struct Scanner {
 
 impl Scanner {
     pub fn new(token_file_path: &str) -> Self {
-        let mut scanner = Scanner {
-            tokens: vec![],
+        Self {
+            tokens: Self::parse_token_file(token_file_path),
             token_list: vec![],
             identifier_table: Table::new(),
             constant_table: Table::new(),
-        };
-        scanner.parse_token_file(token_file_path);
-
-        scanner
+        }
     }
 
-    fn parse_token_file(&mut self, file_path: &str) {
-        //println!("Parsing token file '{}'", &file_path);
+    fn parse_token_file(file_path: &str) -> Vec<String> {
+        println!("Reading '{}'", &file_path);
         let token_file = File::open(file_path).expect("could not open token file");
         let token_reader = io::BufReader::new(token_file);
 
-        let raw_tokens = token_reader
+        token_reader
             .lines()
             .map(|line| line.expect("could not read token line"))
-            .collect::<Vec<String>>();
-
-        //println!("Raw tokens: {:?}", raw_tokens);
-        self.tokens = raw_tokens;
-    }
-
-    pub fn scan(&mut self, file: &str) -> Result<(), String> {
-        println!("Scanning '{}'", file);
-        let program_file = File::open(file).expect("could not open program file");
-        let program_reader = io::BufReader::new(program_file);
-
-        let mut line_number = 1;
-        for line in program_reader.lines() {
-            let line = line.expect("could not read program line");
-
-            match self.parse_line(&line, line_number) {
-                Ok(_) => (),
-                Err(e) => return Err(e),
-            }
-
-            line_number += 1;
-        }
-
-        Ok(())
+            .collect()
     }
 
     pub fn display(&self) {
@@ -70,7 +44,7 @@ impl Scanner {
         for entry in &self.token_list {
             println!("Token: {}, Table Index: {}", entry.key, entry.value);
         }
-        println!("Token table size: {}", self.token_list.len());
+        println!("Token list size: {}", self.token_list.len());
 
         println!("\nIdentifier table:");
         self.identifier_table.display();
@@ -81,56 +55,35 @@ impl Scanner {
         println!("Constant table size: {}", self.constant_table.size());
     }
 
-    fn validate_token(&mut self, token: &str) -> bool {
-        // 1. Is it a reserved word or a symbol? : self.tokens
-        // 2. Is it an identifier? : uppercase and lowercase letters
-        // 3. Is it a constant? : valid number, string, or char
-        // 4. None of the above, lexical error
+    pub fn scan(&mut self, file_path: &str) -> Result<(), String> {
+        println!("Scanning '{}'", file_path);
+        let program_file = File::open(file_path).expect("could not open program file");
+        let program_reader = io::BufReader::new(program_file);
 
-        let rule_2 = Regex::new(r"^([A-Za-z]+)$").unwrap();
-        let rule_3_number = Regex::new(r"^(((\+|-)?[1-9][0-9]*)|(0))$").unwrap();
-        let rule_3_string_char = Regex::new(r#"^("[^"]*"|'[^']')$"#).unwrap();
-
-        let table_key = token.to_string();
-        if self.tokens.contains(&table_key) || table_key == INTERNAL_SEPARATOR {
-            // check if token is a reserved word or a symbol
-            self.token_list.push(Pair {
-                key: table_key,
-                value: -1,
-            });
-
-            return true;
-        } else if rule_2.is_match(token) {
-            // only add identifier to table if it doesn't exist
-            if self.identifier_table.get(&table_key).is_none() {
-                self.identifier_table.insert(table_key.clone());
-            }
-
-            self.token_list.push(Pair {
-                key: IDENTIFIER_NAME.to_string(),
-                value: self.identifier_table.get(&table_key).unwrap().clone(),
-            });
-
-            return true;
-        } else if rule_3_number.is_match(token) || rule_3_string_char.is_match(token) {
-            // only add constant to table if it doesn't exist
-            if self.constant_table.get(&table_key).is_none() {
-                self.constant_table.insert(table_key.clone());
-            }
-
-            self.token_list.push(Pair {
-                key: CONSTANT_NAME.to_string(),
-                value: self.constant_table.get(&table_key).unwrap().clone(),
-            });
-
-            return true;
-        }
-
-        false
+        program_reader
+            .lines()
+            .enumerate()
+            .map(|(line_number, line)| {
+                let line = line.expect("could not read program line");
+                self.parse_line(&line, line_number + 1)
+            })
+            .collect()
     }
 
     fn parse_line(&mut self, line: &str, line_number: usize) -> Result<(), String> {
-        //println!("\nParsing line {:>2}: '{}'", line_number, line);
+        let tokens = self.tokenize_line(line);
+
+        for token in &tokens {
+            if !self.validate_token(token) {
+                let error = format!("Undefined token on line {}: {}", line_number, token);
+                return Err(error);
+            }
+        }
+
+        Ok(())
+    }
+
+    fn tokenize_line(&mut self, line: &str) -> Vec<String> {
         let mut line_tokens = Vec::new();
 
         let mut token = String::new();
@@ -150,8 +103,8 @@ impl Scanner {
             if (ch == '"' && !is_char) || (ch == '\'' && !is_string) {
                 token.push(ch);
 
-                let is_closer = if ch == '"' { is_string } else { is_char };
-                if is_closer {
+                let is_end_quote = if ch == '"' { is_string } else { is_char };
+                if is_end_quote {
                     handle_token(&mut token);
                 }
 
@@ -208,17 +161,61 @@ impl Scanner {
         if !line_tokens.is_empty() {
             line_tokens.push(String::from(INTERNAL_SEPARATOR));
 
-            // validate the tokens and add them to the tables
-            for token in &line_tokens {
-                if !self.validate_token(token) {
-                    let error = format!("Undefined token on line {}: {}", line_number, token);
-                    return Err(error);
-                }
-            }
-
+            // temporary print statement
             println!("Tokens: {:?}", line_tokens);
         }
 
-        Ok(())
+        line_tokens
+    }
+
+    fn validate_token(&mut self, token: &str) -> bool {
+        // Rules:
+        // 1. Is it a reserved word or a symbol? : self.tokens
+        // 2. Is it an identifier? : uppercase and lowercase letters
+        // 3. Is it a constant? : valid number, string, or char
+        // 4. None of the above, lexical error
+
+        let rule_2 = Regex::new(r"^([A-Za-z]+)$").unwrap();
+        let rule_3_number = Regex::new(r"^(((\+|-)?[1-9][0-9]*)|(0))$").unwrap();
+        let rule_3_string_char = Regex::new(r#"^("[^"]*"|'[^']')$"#).unwrap();
+
+        // TODO: verify if char is only one character and if identifier exists in table?
+
+        let table_key = token.to_string();
+        if self.tokens.contains(&table_key) || table_key == INTERNAL_SEPARATOR {
+            // check if token is a reserved word or a symbol
+            self.token_list.push(Pair {
+                key: table_key,
+                value: -1,
+            });
+
+            true
+        } else if rule_2.is_match(token) {
+            // only add identifier to table if it doesn't exist
+            if self.identifier_table.get(&table_key).is_none() {
+                self.identifier_table.insert(table_key.clone());
+            }
+
+            self.token_list.push(Pair {
+                key: String::from(IDENTIFIER_NAME),
+                value: self.identifier_table.get(&table_key).unwrap().clone(),
+            });
+
+            true
+        } else if rule_3_number.is_match(token) || rule_3_string_char.is_match(token) {
+            // only add constant to table if it doesn't exist
+            if self.constant_table.get(&table_key).is_none() {
+                self.constant_table.insert(table_key.clone());
+            }
+
+            self.token_list.push(Pair {
+                key: String::from(CONSTANT_NAME),
+                value: self.constant_table.get(&table_key).unwrap().clone(),
+            });
+
+            true
+        } else {
+            false
+        }
     }
 }
